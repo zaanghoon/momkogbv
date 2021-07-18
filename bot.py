f92fe8b9bf6dee124dcdd69e09955692663c9dda
zaanghoon <87562380+zaanghoon@users.noreply.github.com>
2021-07-19 06:09:17 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_ready():
      # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
      await client.change_presence(status=discord.Status.online)
 
-     await client.change_presence(activity=discord.Game(name=""))
+     #await client.change_presence(activity=discord.Game(name=""))
      #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
      #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
      #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
